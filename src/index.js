2286cb4383b0bcf782f56337fbe15b8901b7f3eb
Foyez <foyezar@gmail.com>
2018-10-29 13:06:51 +0600

    Example branch

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import './utilities'
+import otherSqaure, { add, name } from './utilities'
+import scream from './scream'
+
+console.log('index.js')
+console.log(add(4, 5))
+console.log(scream(name))
+console.log(otherSqaure(10))
